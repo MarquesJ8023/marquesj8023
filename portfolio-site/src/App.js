af24f572f693386d1162f2f6ff4de5d10c3dfd99
Marques <marques.johnson1992@gmail.com>
2019-10-29 22:45:34 -0400

    Nav bar finished, Footer created, Routes to be made

---
+++ portfolio-site/src/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import './App.css';
+
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "Marques Johnson",
+      headerLinks: [
+        {title: "Home", path: '/'},
+        {title: "About", path: '/about'},
+        {title: "Contact", path: '/contact'},
+        {title: "Skills", path: '/skills'}
+      ],
+
+      home: {
+        title: "Progress",
+        subTitle: "Projects that matter",
+        text: "Check out projects below"
+      },
+
+      about: {
+        title: "About Me",
+      },
+
+      contact: {
+        title: "Let's Talk",
+      },
+
+      skills: {
+        title: "Tecnical Skills",
+      }
+
+    };
+  }
+  render() {
+    return (
+      <Router>
+        <Container className='p-o' fluid={true}>
+
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Marques Johnson</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+            <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/skills">Skills</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse> 
+          </Navbar>
+
+          <Route path='/' exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path='/' exact render={} />
+          <Route path='/' exact render={} />
+          <Route path='/' exact render={} />
+
+
+          <Footer />
+
+        </Container>
+
+      </Router>
+      );
+  }
+ 
+}
+
+export default App;
